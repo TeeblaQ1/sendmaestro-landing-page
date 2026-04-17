754f2e9b2fb0cee5a235f75563872a7f276e6645
TeeblaQ1 <oluwateeblaq@gmail.com>
2026-04-18 00:31:34 +0100

    Add @vercel/analytics dependency and integrate Analytics component in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import LandingPage from './containers/LandingPage';
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
-  return (<LandingPage/>);
+  return (
+  <>
+  <LandingPage/>
+  <Analytics />
+  </>
+  );
 }
 
 export default App;
